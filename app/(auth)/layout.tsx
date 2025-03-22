86801165f2acec1f33d1b4847e96d56692f5b93c
Juan Carlos Román <>
2025-03-22 00:43:22 -0700

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="auth-layout">{children}</div>
+  );
+}
+
+export default AuthLayout;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Home page!</h1>
+    </div>
+  );
+}
+
+export default Home;
